cedcdf1b5616f29cdda4c2c1d955b044c658468a
Spiderwoman-code <agnes270909@gmail.com>
2025-03-16 13:28:54 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+    let scale = 1;
+    const card = document.querySelector(".card");
+
+    window.addEventListener("wheel", function (event) {
+        if (event.deltaY > 0) {
+            scale -= 0.1; // Zoom out
+        } else {
+            scale += 0.1; // Zoom in
+        }
+
+        scale = Math.min(Math.max(0.5, scale), 2); // Limit zoom range (0.5x to 2x)
+        card.style.transform = `scale(${scale})`;
+    });
+
